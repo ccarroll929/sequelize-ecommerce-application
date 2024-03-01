82e3c158a10e2b49ae27d4a996797b2ea00288da
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-03-01 17:06:34 -0600

    debugging/fixing typo

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,23 +7,25 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     id: {
-      type: DataTypes.INTEGER, 
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
     product_id: {
-      types: DataTypes.INTEGER, 
+      type: DataTypes.INTEGER,
       references: {
         model: 'product',
-        key: 'id'
+        key: 'id',
+        unique: false
       }
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'tag',
-        key: 'id'
+        key: 'id',
+        unique: false
       }
     }
   },
